ba8838d22b8f65f1a142ccb89fb66318b34a7c1d
mfarhadhussain <farhadh@iisc.ac.in>
2024-05-19 23:02:29 +0530

    update

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ DISPARITY_IMAGE = plt.imread(DISPARITY_IMAGE_PATH)
 ####   CAMERA FRAME #####
 
 # camera intrinsic matrix
-CAMERA_INTRINSIC = np.array([
+CAMERA_INTRINSIC_MATRIX = np.array([
                     [398.7956237792969,0.0,315.4751892089844],
                     [0.0,398.7956237792969,205.1688232421875],
                     [0,0,1]
@@ -22,12 +22,15 @@ CAMERA_INTRINSIC = np.array([
 BASELINE = 0.075 #m
 
 # Since each channel of disparity (400,640,3) is same. So using first channel 
-point_cloud = utils.disparity_to_point(DISPARITY_IMAGE[:,:,0], CAMERA_INTRINSIC, BASELINE)
-point_cloud_scaled = point_cloud/np.max(point_cloud, axis=0) # scaled between 0 and 1 
+THRESOLD = (np.mean(DISPARITY_IMAGE[:,:,0]) - 0.5*np.std(DISPARITY_IMAGE[:,:,0]))
+points_cloud_camera = utils.disparity_to_point(DISPARITY_IMAGE[:,:,0], CAMERA_INTRINSIC_MATRIX, BASELINE, thresold=THRESOLD)
+
+points_cloud_camera_scaled = (points_cloud_camera-np.min(points_cloud_camera))/(np.max(points_cloud_camera)-np.min(points_cloud_camera))
+
 
 fig = plt.figure(figsize=(640/50, 400/50))
 ax = fig.add_subplot(111, projection='3d')
-sc = ax.scatter(point_cloud_scaled[:, 0], point_cloud_scaled[:, 1], point_cloud_scaled[:, 2], c=point_cloud_scaled[:, 0], cmap='plasma', s=1, alpha=1.0)
+sc = ax.scatter(points_cloud_camera_scaled[:, 0], points_cloud_camera_scaled[:, 1], points_cloud_camera_scaled[:, 2], c=points_cloud_camera_scaled[:, 2], cmap='plasma', s=1, alpha=1.0)
 
 cbar = plt.colorbar(sc, ax=ax, pad=0.1)
 cbar.set_label("Depth")
@@ -35,7 +38,7 @@ ax.set_title("Point Cloud in Camera Frame")
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
 ax.set_zlabel("Z")
-ax.view_init(elev=0, azim=180)
+ax.view_init(elev=-90, azim=90)
 # plt.savefig("Assignment/camera_frame.png")
 
 plt.show()
@@ -52,12 +55,12 @@ T_BF_CAM= [
            [ 0.0, -1.0, 0.0,  0.28]
            ]
 
-point_cloud_base = utils.transform_points(transformation_matrix=T_BF_CAM, point_cloud=point_cloud)
-point_cloud_base = point_cloud_base/np.max(point_cloud_base, axis=0) # scaling between 0 and 1
+point_cloud_base = utils.transform_points(transformation_matrix=T_BF_CAM, point_cloud=points_cloud_camera)
+points_cloud_base_scaled = (point_cloud_base-np.min(point_cloud_base))/(np.max(point_cloud_base)-np.min(point_cloud_base))
 
 fig = plt.figure(figsize=(640/50, 400/50))
 ax = fig.add_subplot(111, projection='3d')
-sc = ax.scatter(point_cloud_base[:, 0], point_cloud_base[:, 1], point_cloud_base[:, 2], c=point_cloud_base[:, 0], cmap='plasma', s=1, alpha=1.0)
+sc = ax.scatter(points_cloud_base_scaled[:, 0], points_cloud_base_scaled[:, 1], points_cloud_base_scaled[:, 2], c=points_cloud_base_scaled[:, 0], cmap='plasma', s=1, alpha=1.0)
 cbar = plt.colorbar(sc, ax=ax, pad=0.1)
 cbar.set_label("Depth")
 ax.set_title("Point Cloud in Base Frame")
--- utils.py
+++ utils.py
@@ -8,25 +8,24 @@ import numpy as np
                                 ]
 """
 
-# def disparity_to_point(disparity, camera_intrinsic_matrix, baseline):
+# def disparity_to_point(disparity, camera_intrinsic_matrix, baseline, thresold=None):
 #     f_x = camera_intrinsic_matrix[0, 0]
-#     f_y = camera_intrinsic_matrix[1,1]
-#     u_0 = camera_intrinsic_matrix[0, 2]
-#     v_0 = camera_intrinsic_matrix[1, 2]
+#     f_y = camera_intrinsic_matrix[1, 1]
+#     c_x = camera_intrinsic_matrix[0, 2]
+#     c_y = camera_intrinsic_matrix[1, 2]
     
 #     n_rows, n_columns = disparity.shape
 #     points = np.zeros((n_rows * n_columns, 3))
-
-#     mean_disparity = np.mean(disparity)
-#     std_disparity = np.std(disparity)
+    # if thresold == None:
+    #     thresold = disparity.min()
 
 #     for v in range(n_rows):
 #         for u in range(n_columns):
 #             d = disparity[v, u]
-#             if d > (mean_disparity - 0.5*std_disparity):
+#             if d > thresold:
 #                 z = (f_x * baseline) / d
-#                 x = (u - u_0) * z / f_x
-#                 y = (v - v_0) * z / f_y
+#                 x = (u - c_x) * z / f_x
+#                 y = (v - c_y) * z / f_y
 #                 points[v * n_columns + u]  = np.array([x, y, z])
 #             else:
 #                 points[v * n_columns + u]  = np.array([np.nan, np.nan, np.nan])
@@ -34,25 +33,24 @@ import numpy as np
 #     return points[~np.isnan(points).any(axis=1)]
 
 
-def disparity_to_point(disparity, camera_intrinsic_matrix, baseline):
+def disparity_to_point(disparity, camera_intrinsic_matrix, baseline, thresold=None):
     f_x = camera_intrinsic_matrix[0, 0]
     f_y = camera_intrinsic_matrix[1, 1]
-    u_0 = camera_intrinsic_matrix[0, 2]
-    v_0 = camera_intrinsic_matrix[1, 2]
+    c_x = camera_intrinsic_matrix[0, 2]
+    c_y = camera_intrinsic_matrix[1, 2]
+
+    if thresold == None:
+        thresold = disparity.min()
     
     n_rows, n_columns = disparity.shape
     points = []
-
-    mean_disparity = np.mean(disparity)
-    std_disparity = np.std(disparity)
-
     for v in range(n_rows):
         for u in range(n_columns):
             d = disparity[v, u]
-            if d > (mean_disparity - 0.5 * std_disparity):
+            if d > thresold:
                 z = (f_x * baseline) / d
-                x = (u - u_0) * z / f_x
-                y = (v - v_0) * z / f_y
+                x = (u - c_x) * z / f_x
+                y = (v - c_y) * z / f_y
                 points.append([x, y, z])
                 
     return np.array(points)
